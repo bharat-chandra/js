360bfe55cba8740f81ab8768c242446b0ed4a69c
bharat-chandra <53616992+bharat-chandra@users.noreply.github.com>
2020-09-03 16:15:59 +0530

    Update min-sec.js

--- min-sec.js
+++ min-sec.js
@@ -1,1 +1,19 @@
 
+<html>
+   <head>
+      <title>JavaScript Dates</title>
+   </head>
+   <body>
+      <script>
+         function myTime(time) {
+            var hr = ~~(time / 3600);
+            var min = ~~((time % 3600) / 60);
+            var sec = time * 60;
+            var sec_min = "";
+           
+            return time+" minutes = "+sec+" seconds";
+         }
+         document.write(myTime(2.5));
+      </script>
+   </body>
+</html>
