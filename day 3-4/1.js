1dbee813155ede2f40b72941c2e8d5a68c55babe
bharat-chandra <53616992+bharat-chandra@users.noreply.github.com>
2020-09-11 19:18:49 +0530

    Create 1.js

---
+++ day 3-4/1.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<img id="myImage" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRuTUq-ivbf0kyJCSaGH1qVSXXTr5xmbkDzeA&usqp=CAU" style="width:100px"><br>
+
+<button onclick="document.getElementById('myImage').src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRuTUq-ivbf0kyJCSaGH1qVSXXTr5xmbkDzeA&usqp=CAU'">b1</button>
+<button onclick="document.getElementById('myImage').src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQiwuqLASH4bZooCwgBdxgdYcDXtVR65S6nkA&usqp=CAU'">b2</button>
+<button onclick="document.getElementById('myImage').src='https://static.thenounproject.com/png/8307-200.png'">b3</button>
+
+</body>
+</html>
